18b1fd1dce32185c412c80996932e9cc94f2cad9
NimaiKraus <nimai.kraus@gmail.com>
2023-12-25 23:32:46 +0100

    removed unused import

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,14 +2,13 @@ import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeli
 import "react-vertical-timeline-component/style.min.css";
 
 import { experiences, skills } from "../constants";
-import { Link } from "react-router-dom";
 import CTA from "./CTA";
 
 const About = () => {
   return (
     <section className="max-container">
       <h1 className="head-text">
-        Hello, I'm{" "}
+        Hello, I&apos;m{" "}
         <span className="blue-gradient_text font-semibold drop-shadow">
           {" "}
           Nimai
@@ -47,14 +46,14 @@ const About = () => {
         <h3 className="subhead-text">Work Experience.</h3>
         <div className="mt-5 flex flex-col gap-3 text-slate-500">
           <p>
-            I've worked with all sorts of companies, leveling up my skills and
-            teaming up with smart people. Here's the rundown:
+            I&apos;ve worked with all sorts of companies, leveling up my skills and
+            teaming up with smart people. Here&apos;s the rundown:
           </p>
         </div>
 
         <div className="mt-12 flex">
           <VerticalTimeline>
-            {experiences.map((experience, index) => (
+            {experiences.map((experience) => (
               <VerticalTimelineElement
                 key={experience.company_name}
                 date={experience.date}
